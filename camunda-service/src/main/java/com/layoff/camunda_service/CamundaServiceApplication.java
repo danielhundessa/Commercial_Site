2ae7242c2c6e1e9d1b67f564e9583b6902191372
Daniel Saka <danielhundessa@yahoo.com>
2025-11-11 08:18:59 -0500

    Initialized camunda and notification service using Start.Spring.io

---
+++ camunda-service/src/main/java/com/layoff/camunda_service/CamundaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.layoff.camunda_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaServiceApplication.class, args);
+	}
+
+}
---
+++ camunda-service/src/test/java/com/layoff/camunda_service/CamundaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.layoff.camunda_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
